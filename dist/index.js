d17b15a9cb7a8fc35ad337545db7663fb5c0c4f8
Eugen Degtyarev <ujnatg@gmail.com>
2024-03-23 00:59:42 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -30336,9 +30336,8 @@ async function run() {
   await (0,backoff.backOff)(() => checkWARPRegistration(organization, true), {
     numOfAttempts: 20,
   });
-  await exec.exec("warp-cli", ["--accept-tos", "registration"]);
-  await exec.exec("warp-cli", ["--accept-tos", "registration new"]);
-  await exec.exec("warp-cli", ["--accept-tos", "registration show"]);
+  await exec.exec("warp-cli", ["--accept-tos", "registration",  "new"]);
+  await exec.exec("warp-cli", ["--accept-tos", "registration",  "show"]);
   await exec.exec("warp-cli", ["--accept-tos", "connect"]);
   await (0,backoff.backOff)(() => checkWARPConnected(), { numOfAttempts: 20 });
   core.saveState("connected", "true");
